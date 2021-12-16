bcab98f0258729b0f9e71bbcb158ce2328156030
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2021-12-16 00:31:00 -0600

    new

--- script.js
+++ script.js
@@ -17,13 +17,63 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-const symbols = ['!', '@', '#', '$', '%', '^', '&', '*']
+var includeLowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var includeUppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var includeNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var includeSymbols = ['!', '@', '#', '$', '%', '^', '&', '*']
+var possibleCharacters = []
 
 //window.prompt("How long would you like your password to be?")
 //var length = (Number(prompt("Choose password length between 8 - 128 characters."));
+
+
+var includeLowercaseLetters = false;
+if (confirm('Would you like to include lowercase letters?')) {
+  includeLowercaseLetters = true;
+}
+if (true) {
+  var possibleCharacters = [includeLowercaseLetters]
+}
+
+if (includeLowercaseLetters) {
+  // push all the uppercase letters into possibleCharacters
+}
+
+var includeUppercaseLetters = false;
+if (confirm('Would you like to include uppercase letters?')) {
+  includeUppercaseLetters = true;
+}
+
+var possibleCharacters = [];
+
+if (includeUppercaseLetters) {
+  // push all the uppercase letters into possibleCharacters
+}
+
+var includeNumbers = false;
+if (confirm('Would you like to include uppercase letters?')) {
+  includeNumbers = true;
+}
+
+var possibleCharacters = [];
+
+if (includeNumbers) {
+  // push all the uppercase letters into possibleCharacters
+}
+
+var includeSymbols = false;
+if (confirm('Would you like to include uppercase letters?')) {
+  includeSymbols = true;
+}
+
+var possibleCharacters = [];
+
+if (includeSymbols) {
+  // push all the uppercase letters into possibleCharacters
+}
+
+
+
 
 
 if (confirm('Would you like to include lowercase letters?')) {
@@ -32,9 +82,6 @@ function random_lowerCase(lowerCase) {
   return lowerCase[Math.floor(Math.random()*lowerCase.length)];
 }
   console.log(random_lowerCase(lowerCase));
-   
-//  else {
-//    break;
 
 
 if (confirm('Would you like to include uppercase letters?')) {
